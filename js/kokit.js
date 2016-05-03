1f8588577c9021fedd3d708eda5a70c938795131
Ryan Ko <Ryan.Ko@akqa.com>
2016-05-03 15:17:38 -0700

    Added jsLint updates and verticalScrollTo method

--- js/kokit.js
+++ js/kokit.js
@@ -1,120 +1,168 @@
-var $$ = (function(){
-  var $$ = function(elems){
-    return new KoKit(elems);
-  },
+'use strict';
 
-  KoKit = function(elems) {
-    var domArray = document.querySelectorAll(elems);
-    this.collection = domArray[1] ? Array.prototype.slice.call(domArray) : [domArray];
-    return this;
-  },
+var $ = (function(){
+	var $$,
+		KoKit,
+		Util;
 
-  Util = {
-    getDomIdentifier: function(el) {
-      var domIdentifier = '';
-      if (el.id === '') {
-        var classNameLabelArray = el.className.split(' ');
-        for (var i = 0; i < classNameLabelArray.length; i++) {
-          domIdentifier += '.' + classNameLabelArray[i] + '';
-        }
-      } else {
-        var id = el.id;
-        domIdentifier = '#' + id;
-      }
+	$$ = function(elems){
+		return new KoKit(elems);
+	};
 
-      return domIdentifier;
-    }
-  };
+	KoKit = function(elems) {
+		var domArray = document.querySelectorAll(elems);
+		this.collection = domArray[1] ? Array.prototype.slice.call(domArray) : [domArray];
+	};
 
-  $$.fn = KoKit.prototype = {
-    each : function(fn) {
-      var elems = this.collection;
-      for (var i = 0, l = elems.length; i < l; i++) {
-        fn( elems[i], i );
-      }
-      return this;
-    },
-    css : function(styles) {
-      var elems = this.collection;
-      for (var i = 0, l = elems.length; i < l; i++) {
-        for (var prop in styles) {
-          elems[i].style[prop] = styles[prop];
-        }
-      }
-      return this;
-    },
-    hasClass : function(className) {
-      // http://jaketrent.com/post/addremove-classes-raw-javascript/
-      var elems = this.collection;
+	Util = {
+		getDomIdentifier: function(el) {
+			var domIdentifier = '',
+				classNameLabelArray,
+				i,
+				id;
 
-      for (var i = 0; i < elems.length; i++) {
-        var elem = this.collection.length > 1 ? elems[i] : elems[i][0];
-        if (elem.classList) {
-          return elem.classList.contains(className);
-          break;
-        } else {
-          return !!elem.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
-        }
-      }
-    },
-    addClass : function(classNames) {
-      // http://jaketrent.com/post/addremove-classes-raw-javascript/
-      var elems = this.collection;
-      var that = this;
+			if (el.id === '') {
+				classNameLabelArray = el.className.split(' ');
+				for (i = 0; i < classNameLabelArray.length; i++) {
+					domIdentifier += '.' + classNameLabelArray[i] + '';
+				}
+			} else {
+				id = el.id;
+				domIdentifier = '#' + id;
+			}
 
-      // Loop thru each DOM elements
-      for (var i = 0; i < elems.length; i++) {
-        var el = this.collection.length > 1 ? elems[i] : elems[i][0];
-        // Check if classList is available
-        if (el.classList) {
+			return domIdentifier;
+		}
+	};
 
-          // Create an array of classList
-          var classNamesArray = classNames.split(' ');
-          // For each class, add it via classList
-          for (var j = 0; j < classNamesArray.length; j++) {
-            el.classList.add(classNamesArray[j]);
-          }
+	$$.fn = KoKit.prototype = {
+		// $('.boxes').each(function(target, index){console.log('target.id', target.id);console.log('index', index);});
+		each : function(fn) {
+			var elems = this.collection,
+				i,
+				l;
 
-          // If doesn't support classList, just append as string
-        } else {
-          var target = $$(Util.getDomIdentifier(el));
-          if (!that.hasClass.call(target, classNames)) {
-            el.className += " " + classNames;
-          }
-        }
-      }
-      return this;
-    },
-    removeClass: function(classNames) {
-      // http://jaketrent.com/post/addremove-classes-raw-javascript/
-      var elems = this.collection;
-      var that = this;
+			for (i = 0, l = elems.length; i < l; i++) {
+				fn( elems[i], i );
+			}
+			return this;
+		},
+		// $('.boxes').css({'background':'red', 'color':'#fff'})
+		css : function(styles) {
+			var elems = this.collection,
+				i,
+				l,
+				prop;
 
-      // Loop thru each DOM elements
-      for (var i = 0; i < elems.length; i++) {
-        var el = this.collection.length > 1 ? elems[i] : elems[i][0];
-        // Check if classList is available
-        if (el.classList) {
+			for (i = 0, l = elems.length; i < l; i++) {
+				for (prop in styles) {
+					elems[i].style[prop] = styles[prop];
+				}
+			}
+			return this;
+		},
+		// Same as jQuery
+		hasClass : function(className) {
+			// http://jaketrent.com/post/addremove-classes-raw-javascript/
+			var elems = this.collection,
+				i,
+				elem;
 
-          // Create an array of classList
-          var classNamesArray = classNames.split(' ');
-          // For each class, add it via classList
-          for (var j = 0; j < classNamesArray.length; j++) {
-            el.classList.remove(classNamesArray[j]);
-          }
+			for (i = 0; i < elems.length; i++) {
+				elem = this.collection.length > 1 ? elems[i] : elems[i][0];
+				if (elem.classList) {
+					return elem.classList.contains(className);
+					break;
+				} else {
+					return !!elem.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+				}
+			}
+		},
+		// Same as jQuery
+		addClass : function(classNames) {
+			// http://jaketrent.com/post/addremove-classes-raw-javascript/
+			var elems = this.collection,
+				that = this,
+				i,
+				classNamesArray,
+				j,
+				target;
 
-          // If doesn't support classList, just append as string
-        } else {
-          var target = $$(Util.getDomIdentifier(el));
-          if (!that.hasClass.call(target, classNames)) {
-            var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
-            el.className = el.className.replace(reg, ' ');
-          }
-        }
-      }
-      return this;
-    }
-  };
+			// Loop thru each DOM elements
+			for (i = 0; i < elems.length; i ++) {
+				var el = this.collection.length > 1 ? elems[i] : elems[i][0];
+				// Check if classList is available
+				if (el.classList) {
 
-  return $$;
+					// Create an array of classList
+					classNamesArray = classNames.split(' ');
+					// For each class, add it via classList
+					for (j = 0; j < classNamesArray.length; j++) {
+						el.classList.add(classNamesArray[j]);
+					}
+
+					// If doesn't support classList, just append as string
+				} else {
+					target = $(Util.getDomIdentifier(el));
+					if (!that.hasClass.call(target, classNames)) {
+						el.className += " " + classNames;
+					}
+				}
+			}
+			return this;
+		},
+		// Same as jQuery
+		removeClass: function(classNames) {
+			// http://jaketrent.com/post/addremove-classes-raw-javascript/
+			var elems = this.collection,
+				that = this,
+				i,
+				el,
+				classNamesArray,
+				j,
+				target,
+				reg;
+
+			// Loop thru each DOM elements
+			for (i = 0; i < elems.length; i++) {
+				el = this.collection.length > 1 ? elems[i] : elems[i][0];
+				// Check if classList is available
+				if (el.classList) {
+
+					// Create an array of classList
+					classNamesArray = classNames.split(' ');
+					// For each class, add it via classList
+					for (j = 0; j < classNamesArray.length; j++) {
+						el.classList.remove(classNamesArray[j]);
+					}
+
+					// If doesn't support classList, just append as string
+				} else {
+					target = $(Util.getDomIdentifier(el));
+					if (!that.hasClass.call(target, classNames)) {
+						reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+						el.className = el.className.replace(reg, ' ');
+					}
+				}
+			}
+			return this;
+		},
+		verticalScrollTo: function(yScrollValue) {
+			var elems = this.collection,
+				i,
+				elemsCount = elems.length;
+
+			console.log('elems', elems);
+
+			for (i = 0; i < elemsCount; i++) {
+				if (elemsCount === 1) {
+					elems[i][0].scrollTop = yScrollValue;
+				} else if (elemsCount > 1) {
+					elems[i].scrollTop = yScrollValue;
+				}
+			}
+		}
+	};
+
+	return $$;
 })();
